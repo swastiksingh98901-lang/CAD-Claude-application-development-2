f7d851775b5221ff6e7510fa4e7162141c3426de
Swastik singh <swastiksingh98901@bbdu.ac.in>
2026-04-01 17:12:35 +0530

    Add multiplication functionality in Multipication.py

---
+++ Multipication.py
@@ -0,0 +1,8 @@
+num1_str = input("Enter the first number: ")
+num2_str = input("Enter the second number: ")
+
+num1 = float(num1_str)
+num2 = float(num2_str)
+product = num1 * num2
+
+print(f"The product of {num1} and {num2} is {product}")
